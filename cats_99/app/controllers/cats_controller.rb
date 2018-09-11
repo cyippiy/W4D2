8dfc17af18bab5202a93fc6e7eaf8251f3db3464
cyip <chris.yip87@gmail.com>
2018-09-11 15:22:48 -0700

    Complete Cat Resources

--- cats_99/app/controllers/cats_controller.rb
+++ cats_99/app/controllers/cats_controller.rb
@@ -6,6 +6,49 @@ class CatsController < ApplicationController
   
   def show 
     @cats = Cat.find(params[:id])
-    render :show
+    
+    if @cats 
+      render :show
+    else 
+      redirect_to cats_url
+    end 
+  end 
+  
+  def new 
+    @cat = Cat.new()
+    render :new 
+  end 
+  
+  def create 
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      redirect_to cat_url(@cat) 
+    else 
+      render :new
+    end 
+  end 
+  
+  def update
+    @cat = Cat.find(params[:id])
+    
+    if @cat.update(cat_params)
+      redirect_to cat_url(@cat)
+    else 
+      render json: @cat.errors.full_messages, status: 422
+    end 
+  end 
+  
+  def edit 
+    @cat = Cat.find(params[:id])
+    if @cat
+      render :edit
+    else 
+      redirect_to cats_url 
+    end 
+  end 
+  
+  private 
+  def cat_params
+    params.require(:cat).permit(:birth_date, :color, :name, :sex, :description)
   end 
 end 
